15ed34a362c89d1d75418e430a829d103d1415d9
Kriti bansal <kritibansal318@gmail.com>
2026-08-15 10:40:20 +0530

    add user input

---
+++ backend/main.py
@@ -0,0 +1,8 @@
+# input of the repo and the task to perform in the repo
+repo = input("GitHub Repository: ")   # TASK :-- validator -- repo is in the right format and validity check 
+task = input("Task: ")
+
+print("\nRepository:", repo)
+print("Task:", task)
+
+
